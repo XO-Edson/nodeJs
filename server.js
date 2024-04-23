c683b5eeb91a6ffc1c928c7798d8567e9b63ef68
Shaiyo <edsonmike053@gmail.com>
2024-04-23 12:54:30 +0300

    MVC REST api

--- server.js
+++ server.js
@@ -3,33 +3,17 @@ const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logEvents");
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const errHandler = require("./middleware/errorLog");
 
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
 
-const whiteList = [
-  "https://www.google.com",
-  "http://127.0.0.1:5500",
-  "http://localhost3500",
-];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.use("/subdir", express.static(path.join(__dirname, "/public")));
